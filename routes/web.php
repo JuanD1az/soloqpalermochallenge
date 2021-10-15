57e38eca3155cfde4de8892b1d6a3cf0fa5815f3
JuanDiaz1 <juanalbertodiazparra@gmail.com>
2021-10-15 18:35:19 -0300

    segundo commit v4

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/', 'App\Http\Controllers\ParticipantesController');
+Route::resource('test', 'App\Http\Controllers\ParticipantesController');
