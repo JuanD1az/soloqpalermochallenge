b7d691dd2e8a0272440a21479d2647b036e1e7f3
JuanDiaz1 <juanalbertodiazparra@gmail.com>
2021-10-16 16:36:18 -0300

    segundo commit v16

--- app/Http/Controllers/ParticipantesController.php
+++ app/Http/Controllers/ParticipantesController.php
@@ -19,7 +19,7 @@ class ParticipantesController extends Controller
         $array = [];
         foreach($participantes as $p => $key){
             $array[$key->id] = Participante::find($key->id);
-            $response = Http::get("https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/$key->iduser?api_key=RGAPI-8b83b970-c981-4bf8-9855-d4647501b894");
+            $response = Http::get("https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/$key->iduser?api_key=RGAPI-45faf019-6771-4281-9cd8-73908842c684");
             $datos = json_decode($response->getBody());
             $array[$key->id] = $datos;
             $array[$key->id][5]['nombre']=$key->nickname;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,18 +10,38 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <title>SoloQ Palermo Challenge</title>
+
+    <style>
+        thead{
+            background:linear-gradient(274deg, #47919d, #0a2830, #2e636c, #0a2830);
+            border:0px solid transparent;
+            background-size: 400% 400%;
+            color:#a3a071;
+        }
+        td{
+            border: 0px solid transparent;
+        }
+        .tdn {
+            line-height: 250%;
+        }
+        body{
+            background-color:#121212;
+        }
+        table{
+            border-color:#af7c41 !important;
+            background-color: white;
+            border: 2px solid;
+        }
+    </style>
   </head>
-  <body>
-    <h1>SoloQ Palermo Challenge</h1>
-    <table id="categorias" class="table table-striped table-bordered" style="width:100%">
-        <thead class="table-dark">
+  <body class="container-sm">
+    <h1 class="text-center" style="color:white;">SoloQ Palermo Challenge</h1>
+    <table id="categorias" class="table table-striped table-bordered  text-center" style="width:100%">
+        <thead>
             <tr>
                 <th scope="col">Nombre</th>
                 <th scope="col">Cuenta</th>
                 <th scope="col">Rank</th>
-                <th scope="col">LP</th>
-                <th scope="col">Wins</th>
-                <th scope="col">Losses</th>
                 <th scope="col">Winrate</th>
             </tr>
         </thead>
@@ -29,22 +49,51 @@
 
             @foreach ($array as $participante)
             <tr>            
-                @if (count($participante)<3)
-                    <td>{{$participante[5]['nombre']}}</td>
-                    <td>{{$participante[0]->summonerName}}</td>
-                    <td>{{$participante[0]->tier}} {{$participante[0]->rank}}</td>
-                    <td>{{$participante[0]->leaguePoints}}</td>
-                    <td>{{$participante[0]->wins}}</td>
-                    <td>{{$participante[0]->losses}}</td>
-                    <td>{{number_format($participante[0]->wins/($participante[0]->wins+$participante[0]->losses)*100)}}%</td>
-                @else(count($participante)<2)
-                    <td>{{$participante[5]['nombre']}}</td>
-                    <td>{{$participante[1]->summonerName}}</td>
-                    <td>{{$participante[1]->tier}} {{$participante[1]->rank}}</td>
-                    <td>{{$participante[1]->leaguePoints}}</td>
-                    <td>{{$participante[1]->wins}}</td>
-                    <td>{{$participante[1]->losses}}</td>
-                    <td>{{number_format($participante[1]->wins/($participante[1]->wins+$participante[1]->losses)*100)}}%</td>
+                @if (count($participante)<4)
+                    <td class="tdn">{{$participante[5]['nombre']}}</td>
+                    <td class="tdn">{{$participante[0]->summonerName}}</td>
+                    <td class="card-body">
+                        <h6 class="card-title">{{$participante[0]->tier}} {{$participante[1]->rank}}</h5>
+                        <p class="card-text">({{$participante[0]->leaguePoints}} LP)</p>
+                    </td>
+                    <td>
+                        <h6 class="card-title">
+                        <div class="progress">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: {{number_format($participante[0]->wins/($participante[0]->wins+$participante[0]->losses)*100)}}%">
+                                {{$participante[0]->wins}}
+                            </div>
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: {{number_format($participante[0]->losses/($participante[0]->losses+$participante[0]->wins)*100)}}%">
+                                {{$participante[0]->losses}}
+                            </div>
+                        </div></h5>
+                        <p class="card-text">
+                            {{number_format($participante[0]->wins/($participante[0]->wins+$participante[0]->losses)*100)}}%
+                        </p>
+                    </td>
+
+
+
+                @else(count($participante)<3)
+                    <td class="tdn">{{$participante[5]['nombre']}}</td>
+                    <td class="tdn">{{$participante[1]->summonerName}}</td>
+                    <td class="card-body">
+                        <h6 class="card-title">{{$participante[1]->tier}} {{$participante[1]->rank}}</h5>
+                        <p class="card-text">({{$participante[1]->leaguePoints}} LP)</p>
+                    </td>
+                    <td class="card-body">
+                        <h6 class="card-title">
+                        <div class="progress">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: {{number_format($participante[1]->wins/($participante[1]->wins+$participante[1]->losses)*100)}}%">
+                                {{$participante[1]->wins}}
+                            </div>
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: {{number_format($participante[1]->losses/($participante[1]->losses+$participante[1]->wins)*100)}}%">
+                                {{$participante[1]->losses}}
+                            </div>
+                        </div></h5>
+                        <p class="card-text">
+                            {{number_format($participante[1]->wins/($participante[1]->wins+$participante[1]->losses)*100)}}%
+                        </p>
+                    </td>
                 @endif
             </tr>
             @endforeach
